63324b87a0bc125ffd1054deaf6d916dd4aaeda4
Mehar <meharpreet.sachdeva@iotfy.co>
2026-05-26 18:24:22 +0530

    fix; db not connecting.

--- server/database/db.js
+++ server/database/db.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import dns from "dns";
+// Set DNS servers to Google's public DNS to resolve MongoDB Atlas SRV records
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 export const Connection =async ()=>{
 
